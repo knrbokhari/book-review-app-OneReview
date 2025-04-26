e2842f2da49d8701001bc6294f7e167d3799633f
knrbokhari <kazinaeemrayhan@gmail.com>
2025-04-26 16:52:20 +0600

    project init

---
+++ frontend/next.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.sanity.io",
+        port: ""
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: ""
+      },
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: ""
+      },
+    ]
+  }
+};
+
+export default nextConfig;
